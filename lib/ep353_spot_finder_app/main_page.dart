b54e954c5f295414d86e63df5e06dfaee1ac8f2c
Dreamwalker <aristojeff@gmail.com>
2020-02-06 21:56:05 +0900

    Edits

---
+++ lib/ep353_spot_finder_app/main_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+
+class SpotFinderApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SpotFinderMainPage(),
+    );
+  }
+}
+
+class SpotFinderMainPage extends StatefulWidget {
+  @override
+  _SpotFinderMainPageState createState() => _SpotFinderMainPageState();
+}
+
+class _SpotFinderMainPageState extends State<SpotFinderMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
