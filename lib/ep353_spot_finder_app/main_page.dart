b4e68b807c5041e9f0475292893b479680b6782a
Dreamwalker <aristojeff@gmail.com>
2020-02-06 22:03:47 +0900

    Update main_page.dart

--- lib/ep353_spot_finder_app/main_page.dart
+++ lib/ep353_spot_finder_app/main_page.dart
@@ -18,12 +18,49 @@ class SpotFinderMainPage extends StatefulWidget {
 class _SpotFinderMainPageState extends State<SpotFinderMainPage> {
   @override
   Widget build(BuildContext context) {
+    var deviceHeight =MediaQuery.of(context).size.height;
+    var deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SingleChildScrollView(
         child: SafeArea(
           child: Column(
             children: <Widget>[
-              
+
+              //appbar
+              SizedBox(
+                height: deviceHeight / 10,
+                child: Placeholder()
+              ),
+              //Image
+              SizedBox(
+                  height: deviceHeight / 4,
+                  child: Placeholder()
+              ),
+              //shop information
+              SizedBox(
+                  height: deviceHeight / 6,
+                  child: Placeholder()
+              ),
+
+              //contact
+              SizedBox(
+                  height: deviceHeight / 10,
+                  child: Placeholder()
+              ),
+              // shop detail list
+              SizedBox(
+                  height: deviceHeight / 9,
+                  child: Placeholder()
+              ),
+              SizedBox(
+                  height: deviceHeight / 20,
+                  child: Placeholder()
+              ),
+              SizedBox(
+                  height: deviceHeight / 2.5,
+                  child: Placeholder()
+              ),
+
             ],
           ),
         ),
