f1ca519f0487854a20bf4d74956040290a4a7ae3
Dreamwalker <aristojeff@gmail.com>
2020-02-06 21:47:34 +0900

    Adds

---
+++ android/app/src/main/kotlin/flutter/dreamwalker/com/flutter_notebook_4th/MainActivity.kt
@@ -0,0 +1,12 @@
+package flutter.dreamwalker.com.flutter_notebook_4th
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
