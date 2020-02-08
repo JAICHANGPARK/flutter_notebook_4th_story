889c48a6dc3d518ae7de0cbd11117bd2a76e098e
Dreamwalker <aristojeff@gmail.com>
2020-02-08 21:12:37 +0900

    Create main_page.dart

---
+++ lib/ep355_monochrome_shop_app/main_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MonochromeShopApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MonochromeShopHome(),
+    );
+  }
+}
+
+class MonochromeShopHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
