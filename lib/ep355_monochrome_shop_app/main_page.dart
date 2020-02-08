903b9f3855ddb49719a6f52bcffbf072e6725d1e
Dreamwalker <aristojeff@gmail.com>
2020-02-08 21:16:28 +0900

    Update main_page.dart

--- lib/ep355_monochrome_shop_app/main_page.dart
+++ lib/ep355_monochrome_shop_app/main_page.dart
@@ -13,8 +13,59 @@ class MonochromeShopHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            bottom: 48,
+            right: 24,
+            child: Container(
+              height: 86,
+              width: 86,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all()
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text("explore",style: TextStyle(
+                    fontSize: 16
+                  ),),
+                  SizedBox(height: 4,),
+                  Icon(Icons.arrow_forward,size: 20, ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
