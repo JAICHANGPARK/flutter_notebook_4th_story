eb10f27706ef948956317ded64762cb3a16024c6
Dreamwalker <aristojeff@gmail.com>
2020-02-09 20:15:26 +0900

    Update entered_page.dart

--- lib/ep355_monochrome_shop_app/entered_page.dart
+++ lib/ep355_monochrome_shop_app/entered_page.dart
@@ -1,2 +1,14 @@
 import 'package:flutter/material.dart';
 
+class WomensBlazersPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          
+        ],
+      ),
+    );
+  }
+}
