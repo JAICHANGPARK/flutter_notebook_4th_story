876dbb1cd35117e464c125afc7265fe33b7716fc
Dreamwalker <aristojeff@gmail.com>
2020-02-09 20:22:18 +0900

    Update entered_page.dart

--- lib/ep355_monochrome_shop_app/entered_page.dart
+++ lib/ep355_monochrome_shop_app/entered_page.dart
@@ -4,37 +4,64 @@ class WomensBlazersPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 2,
-            child: Padding(
-              padding: const EdgeInsets.only(right: 16, left: 16),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Icon(
-                    Icons.arrow_back,
-                    color: Colors.grey.shade500,
-                  ),
-                  Text(
-                    "Women's Blazers",
-                    style: TextStyle(fontSize: 18, color: Colors.black87),
-                  ),
-                  Icon(
-                    Icons.search,
-                    color: Colors.grey.shade500,
-                  ),
-                ],
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: const EdgeInsets.only(right: 16, left: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.arrow_back,
+                      color: Colors.grey.shade500,
+                    ),
+                    Text(
+                      "Women's Blazers",
+                      style: TextStyle(fontSize: 18, color: Colors.black87),
+                    ),
+                    Icon(
+                      Icons.search,
+                      color: Colors.grey.shade500,
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          Expanded(
-            flex: 10,
-            child: Placeholder(),
-          )
-        ],
+            Expanded(
+              flex: 13,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: GridView.builder(
+
+                  itemBuilder: (BuildContext context, int index) {  },
+                  gridDelegate: null,
+                  children: <Widget>[
+
+                    Container(
+                      color: Colors.red,
+                    ),
+                    Container(
+                      color: Colors.red,
+                    ),
+                    Container(
+                      color: Colors.red,
+                    ),
+                    Container(
+                      color: Colors.red,
+                    ),
+                    Container(
+                      color: Colors.red,
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
