47c1a787406a80807ffb0ed0f8795aeec3f61dee
Dreamwalker <aristojeff@gmail.com>
2020-02-10 21:14:09 +0900

    Create detail_page.dart

---
+++ lib/ep355_monochrome_shop_app/detail_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
