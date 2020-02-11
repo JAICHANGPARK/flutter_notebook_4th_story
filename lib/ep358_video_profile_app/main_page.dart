a81efe5fcace511a4a1bcc4654dc065f2240c10a
Dreamwalker <aristojeff@gmail.com>
2020-02-11 21:02:31 +0900

    Update main_page.dart

--- lib/ep358_video_profile_app/main_page.dart
+++ lib/ep358_video_profile_app/main_page.dart
@@ -1,2 +1,23 @@
 
 import 'package:flutter/material.dart';
+
+class VideoProfileApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: VPMainPage(),
+    );
+  }
+}
+
+class VPMainPage extends StatefulWidget {
+  @override
+  _VPMainPageState createState() => _VPMainPageState();
+}
+
+class _VPMainPageState extends State<VPMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
