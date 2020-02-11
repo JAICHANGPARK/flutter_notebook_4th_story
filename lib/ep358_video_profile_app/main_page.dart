4269528bc121f3378440f988c4e803ce75689e85
Dreamwalker <aristojeff@gmail.com>
2020-02-11 21:04:55 +0900

    Adds packages

--- lib/ep358_video_profile_app/main_page.dart
+++ lib/ep358_video_profile_app/main_page.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class VideoProfileApp extends StatelessWidget {
   @override
@@ -18,9 +19,38 @@ class VPMainPage extends StatefulWidget {
 class _VPMainPageState extends State<VPMainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SlidingUpPanel(
+        panel: Center(
+          child: Text("This is the sliding Widget"),
+        ),
+        body: Center(
+          child: Text("This is the Widget behind the sliding panel"),
+        ),
+      )
+    );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
