37dad6f53fe15218d8d0815beaddd51c727731e9
Dreamwalker <aristojeff@gmail.com>
2020-02-12 21:18:46 +0900

    Update main_page.dart

--- lib/ep358_video_profile_app/main_page.dart
+++ lib/ep358_video_profile_app/main_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:video_player/video_player.dart';
@@ -19,22 +18,22 @@ class VPMainPage extends StatefulWidget {
 
 class _VPMainPageState extends State<VPMainPage> {
   VideoPlayerController _videoPlayerController;
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
 
     _videoPlayerController = VideoPlayerController.network(
-      'https://player.vimeo.com/external/135732201.mobile.mp4?s=d221063caaaf7da1c2155f00ffd08b56a12436c5&profile_id=116'
-    )..initialize().then((value) {
-      setState(() {
-        _videoPlayerController.play();
+        'https://player.vimeo.com/external/135732201.mobile.mp4?s=d221063caaaf7da1c2155f00ffd08b56a12436c5&profile_id=116')
+      ..initialize().then((value) {
+        setState(() {
+          _videoPlayerController.play();
+        });
+      })
+      ..addListener(() {
+        setState(() {});
       });
-    })..addListener(() {
-      setState(() {
-
-      });
-    });
 
     _videoPlayerController.setLooping(true);
   }
@@ -49,91 +48,73 @@ class _VPMainPageState extends State<VPMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SlidingUpPanel(
-        maxHeight: 620,
-        panel: Center(
-          child: Text("This is the sliding Widget"),
-        ),
-        body: Stack(
-          children: <Widget>[
-            Positioned(
-              left: 0,
-              top: 0,
-              right: 0,
-              bottom: 0,
-              child: _videoPlayerController.value.initialized ? VideoPlayer(_videoPlayerController):
-              Center(
-                child: CircularProgressIndicator(),
-              ),
+        body: SlidingUpPanel(
+            maxHeight: 620,
+            panel: Center(
+              child: Text("This is the sliding Widget"),
             ),
-            Positioned(
-              left: 24,
-              bottom: 140,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text("Mary", style: TextStyle(
-                    fontSize: 48,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),),
-                  Text("Henderson",style: TextStyle(
-                    fontSize: 48,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-
-                  ),),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  Text("Head of Human Resources",style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18
-                  ),),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  Row(
+            body: Stack(
+              children: <Widget>[
+                Positioned(
+                  left: 0,
+                  top: 0,
+                  right: 0,
+                  bottom: 0,
+                  child: _videoPlayerController.value.initialized
+                      ? VideoPlayer(_videoPlayerController)
+                      : Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                ),
+                Positioned(
+                  left: 24,
+                  bottom: 140,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Icon(Icons.add_location, size: 18,
-                      color: Colors.white,),
-                      Text("PURCHASE, NY, USA", style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16
-                      ),)
+                      Text(
+                        "Mary",
+                        style: TextStyle(
+                          fontSize: 48,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        "Henderson",
+                        style: TextStyle(
+                          fontSize: 48,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        "Head of Human Resources",
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.add_location,
+                            size: 18,
+                            color: Colors.white,
+                          ),
+                          Text(
+                            "PURCHASE, NY, USA",
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          )
+                        ],
+                      )
                     ],
-                  )
-                ],
-              ),
-            ),
-          ],
-        )
-      )
-    );
+                  ),
+                ),
+              ],
+            )));
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
