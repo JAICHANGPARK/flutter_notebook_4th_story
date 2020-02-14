3030daf53fa173e8dd56c3b5f69f3d3c9507a7ac
Dreamwalker <aristojeff@gmail.com>
2020-02-14 22:46:22 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_4th/ep358_video_profile_app/main_page.dart';
+import 'package:flutter_notebook_4th/ep361_plant_shop_app/main_page.dart';
 
-void main() => runApp(VideoProfileApp());
+void main() => runApp(PlantShopApp());
