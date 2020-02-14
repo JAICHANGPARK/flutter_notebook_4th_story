dd30b66e6bd71bbf87252db154cd65afb0723a63
Dreamwalker <aristojeff@gmail.com>
2020-02-14 22:45:57 +0900

    Create main_page.dart

---
+++ lib/ep361_plant_shop_app/main_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PlantShopApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: PlantMainPage(),
+    );
+  }
+}
