7600df383ccc22a39533ee5d06c5bc574f9f8d64
Dreamwalker <aristojeff@gmail.com>
2020-02-16 20:24:22 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_4th/ep361_plant_shop_app/main_page.dart';
+import 'package:flutter_notebook_4th/ep363_hire_professional/main_page.dart';
 
-void main() => runApp(PlantShopApp());
+void main() => runApp(HireProfessionalApp());
