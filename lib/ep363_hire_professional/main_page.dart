67a75f3129735465685d3946ecaaac3ea041297d
Dreamwalker <aristojeff@gmail.com>
2020-02-16 20:40:19 +0900

    Update main_page.dart

--- lib/ep363_hire_professional/main_page.dart
+++ lib/ep363_hire_professional/main_page.dart
@@ -20,17 +20,101 @@ class _HPMainPageState extends State<HPMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueGrey.shade50,
       body: Column(
         children: <Widget>[
           Expanded(
             flex: 4,
-            child: Placeholder(),
+            child: Container(
+              color: Colors.white,
+              child: SafeArea(
+                child: Column(
+                  children: <Widget>[
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16,
+                        vertical: 16),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Icon(Icons.menu),
+                            Spacer(),
+                            Text("Find a Professional",style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),),
+                            Spacer(),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            flex: 9,
+                            child: Container(
+                              margin: EdgeInsets.only(left: 16, bottom: 8, top: 8),
+                              decoration: BoxDecoration(
+                                color: Colors.blueGrey.shade50,
+                                borderRadius: BorderRadius.circular(8)
+                              ),
+                              child: Row(
+                                children: <Widget>[
+                                  Expanded(
+                                    flex: 10,
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(left: 8),
+                                      child: TextField(
+                                        decoration: InputDecoration(
+                                          border: InputBorder.none,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    flex: 2,
+                                    child: Container(
+                                      height: double.infinity,
+                                      margin: EdgeInsets.all(4),
+                                      decoration: BoxDecoration(
+                                        color: Colors.blue,
+                                        borderRadius: BorderRadius.circular(8)
+                                      ),
+                                      child: Icon(Icons.search, color: Colors.white,),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: Container(
+                              height: double.infinity,
+                              margin: EdgeInsets.only(left: 16, right: 16, bottom: 8,
+                              top: 8),
+                              decoration: BoxDecoration(
+                                color: Colors.blueGrey.shade50,
+                                borderRadius: BorderRadius.circular(8)
+                              ),
+                              child: Icon(Icons.tune,color: Colors.blueGrey,),
+
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
           ),
           Expanded(
-            flex: 12,
+            flex: 15,
             child: SingleChildScrollView(
               child: ConstrainedBox(
-                constraints: BoxConstraints(maxHeight: MediaQuery.of(context).size.height),
+                constraints: BoxConstraints(maxHeight: MediaQuery.of(context).size.height ),
                 child: Column(
                   children: <Widget>[
                     Expanded(
