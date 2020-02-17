78fd56a9dacb4c539378c71d91ea5cd10e0a8413
Dreamwalker <aristojeff@gmail.com>
2020-02-17 20:55:27 +0900

    Create user.dart

---
+++ lib/ep363_hire_professional/user.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+
+class User{
+  String img;
+  String name;
+  int reviews;
+  String lastestReviews;
+
+  User({this.img, this.name, this.reviews, this.lastestReviews});
+
+
+}
+List<User> hireUsers = [
+
+  User(
+    img: "",
+    name: "Judith Cooper",
+    reviews: 87,
+    lastestReviews: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+  ),
+
+  
+
+
+
+];
