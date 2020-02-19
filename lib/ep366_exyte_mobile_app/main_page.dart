8177dffbbb2a2aaa6339f1e173275a5258e9fb07
Dreamwalker <aristojeff@gmail.com>
2020-02-19 21:19:58 +0900

    Update main_page.dart

--- lib/ep366_exyte_mobile_app/main_page.dart
+++ lib/ep366_exyte_mobile_app/main_page.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ExyteMobileApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home:SecondPage()
-    );
+    return MaterialApp(home: SecondPage());
   }
 }
-
 
 class SecondPage extends StatefulWidget {
   @override
@@ -18,7 +16,6 @@ class SecondPage extends StatefulWidget {
 class _ExyteMobileAppState extends State<SecondPage> {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Colors.grey.shade200,
       body: Stack(
@@ -30,20 +27,54 @@ class _ExyteMobileAppState extends State<SecondPage> {
             right: 0,
             child: Container(
               decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(48),
-                  topRight: Radius.circular(48),
-                ),
-
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.05),
-                    blurRadius: 5,
-                    spreadRadius: 4,
-                    offset: Offset(-4,-8)
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(48),
+                    topRight: Radius.circular(48),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black.withOpacity(0.05),
+                        blurRadius: 5,
+                        spreadRadius: 4,
+                        offset: Offset(-4, -8))
+                  ]),
+              padding: EdgeInsets.only(left: 32, top: 38),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Aerum",
+                    style: GoogleFonts.montserrat().copyWith(fontSize: 22),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(right: 32),
+                    height: 260,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                "https://assets-ouch.icons8.com/thumb/905/cdec4487-c229-4acc-9bfc-0fcae8f06f78.png"),
+                            fit: BoxFit.fitHeight,
+                            colorFilter: ColorFilter.mode(
+                                Colors.white, BlendMode.saturation))),
+                  ),
+                  Text(
+                    "Work process",
+                    style: GoogleFonts.montserrat().copyWith(fontSize: 18),
+                  ),
+                  SizedBox(
+                    height: 9,
+                  ),
+                  Container(
+                    height: MediaQuery.of(context).size.height / 8,
+                    decoration: BoxDecoration(color: Colors.grey),
+                    child: ListView.builder(
+                      itemBuilder: (context, index) {},
+                      itemCount: 4,
+                    ),
                   )
-                ]
+                ],
               ),
             ),
           ),
@@ -52,14 +83,13 @@ class _ExyteMobileAppState extends State<SecondPage> {
             right: 0,
             left: 0,
             child: Container(
-              height: MediaQuery.of(context).size.height / 3,
+              height: MediaQuery.of(context).size.height / 3.3,
               decoration: BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(48),
-                  topRight: Radius.circular(48),
-                )
-              ),
+                  color: Colors.black,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(48),
+                    topRight: Radius.circular(48),
+                  )),
             ),
           )
         ],
@@ -67,22 +97,3 @@ class _ExyteMobileAppState extends State<SecondPage> {
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
