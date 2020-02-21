93c28f6ac2a5dba5383489c70209e6da6c0b0d5e
Dreamwalker <aristojeff@gmail.com>
2020-02-21 20:34:52 +0900

    Update main_page.dart

---
+++ lib/ep368_handmade_app/main_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HandMadeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: "/",
+      routes: {
+        '/' : (context) => HandMadeMainPage(),
+      },
+    );
+  }
+}
+
+
+class HandMadeMainPage extends StatefulWidget {
+  @override
+  _HandMadeAppState createState() => _HandMadeAppState();
+}
+
+class _HandMadeAppState extends State<HandMadeMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
