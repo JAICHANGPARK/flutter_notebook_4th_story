8fcf25b30415b6441fa49ad5eb321a05ea61f286
Dreamwalker <aristojeff@gmail.com>
2020-02-21 20:44:08 +0900

    Update main_page.dart

--- lib/ep368_handmade_app/main_page.dart
+++ lib/ep368_handmade_app/main_page.dart
@@ -21,7 +21,41 @@ class HandMadeMainPage extends StatefulWidget {
 class _HandMadeAppState extends State<HandMadeMainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage("https://cdn.pixabay.com/photo/2015/06/09/20/36/portrait-804062_960_720.jpg"),
+                  fit: BoxFit.cover,
+                )
+              ),
+            ),
+          ),
+          Positioned(
+            left: 16,
+            right: 16,
+            top: 36,
+            child: Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Icon(Icons.arrow_back_ios,color: Colors.white,),
+                  Icon(Icons.add_shopping_cart,color: Colors.white,),
+                ],
+              ),
+            ),
+          ),
+
+        ],
+      ),
+    );
   }
 }
 
