65d13cbd116836a73cb9751fc877585fee5ac78e
Dreamwalker <aristojeff@gmail.com>
2020-02-22 20:57:26 +0900

    Adds

--- lib/ep368_handmade_app/main_page.dart
+++ lib/ep368_handmade_app/main_page.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
+const Color primaryColor =  Color(0xff8c6e6b);
+const Color secondaryColor = Color(0xffC5BCBC);
+
 class HandMadeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -187,20 +190,84 @@ class HandMadeHomePage extends StatefulWidget {
 }
 
 class _HandMadeHomePageState extends State<HandMadeHomePage> {
-  final PageController _pageController = PageController();
+  final PageController _pageController = PageController(
+    viewportFraction: 0.9
+  );
+  List<Widget> _buildBackgroud(){
+    return [
+      Positioned(
+        left: 0,
+        right: 0,
+        top: 0,
+        child: Container(
+          height: MediaQuery.of(context).size.height /2.5,
+          decoration: BoxDecoration(
+              color: Colors.grey,
+              image: DecorationImage(
+                  image: NetworkImage("https://cdn.pixabay.com/photo/2017/03/30/18/17/girl-2189247_960_720.jpg"),
+                  fit: BoxFit.cover,
+                  colorFilter: ColorFilter.mode(
+                    Colors.grey,
+                    BlendMode.color,
+                  )
+              )
+          ),
+        ),
+      ),
+      Positioned(
+        left: 0,
+        right: 0,
+        top: MediaQuery.of(context).size.height /2.5,
+        child: Container(
+          height: MediaQuery.of(context).size.height /3.4,
+          color: Colors.white,
+        ),
+      ),
+      Positioned(
+        left: 0,
+        right: 0,
+        top: MediaQuery.of(context).size.height /2.5 * 1.75,
+        child: Container(
+          height: MediaQuery.of(context).size.height /2.5 ,
+          color: Color(0xffC5BCBC),
+        ),
+      ),
+    ];
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: <Widget>[
+          ..._buildBackgroud(),
           Positioned(
-            left: 0,
+            left: 24,
             right: 0,
-            top: 0,
+            top: MediaQuery.of(context).size.height / 3,
             child: Container(
-              height: MediaQuery.of(context).size.height /3 ,
+              height: 100,
+              child: PageView(
+                controller: _pageController,
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(right: 16),
+                    decoration: BoxDecoration(
+                      color: primaryColor,
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(24),
+                        bottomLeft: Radius.circular(24),
+                      )
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.green
+                    ),
+                  ),
+                ],
+              ),
             ),
-
           )
         ],
       ),
