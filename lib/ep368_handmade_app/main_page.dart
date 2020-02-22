8797fa198df0b5b9935e6a4f1bcd254aaccd73f8
Dreamwalker <aristojeff@gmail.com>
2020-02-22 20:43:19 +0900

    Update main_page.dart

--- lib/ep368_handmade_app/main_page.dart
+++ lib/ep368_handmade_app/main_page.dart
@@ -187,12 +187,26 @@ class HandMadeHomePage extends StatefulWidget {
 }
 
 class _HandMadeHomePageState extends State<HandMadeHomePage> {
+  final PageController _pageController = PageController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            child: Container(
+              height: MediaQuery.of(context).size.height /3 ,
+            ),
+
+          )
+        ],
+      ),
 
 
-      
+
     );
   }
 }
