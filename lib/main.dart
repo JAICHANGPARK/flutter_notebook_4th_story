fc89a0c0f5d75b7fc3c3632c56831b6112309f6f
Dreamwalker <aristojeff@gmail.com>
2020-02-24 21:18:07 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
 import 'package:flutter/material.dart';
-
-import 'package:flutter_notebook_4th/ep368_handmade_app/main_page.dart';
-void main() => runApp(HandMadeApp());
+import 'package:flutter_notebook_4th/ep371_music_player/main_page.dart';
+void main() => runApp(MusicPlayerApp());
