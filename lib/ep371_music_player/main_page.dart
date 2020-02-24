5c4807687567a3d0b4c80abce64ab40771f27ce0
Dreamwalker <aristojeff@gmail.com>
2020-02-24 21:17:29 +0900

    Update main_page.dart

---
+++ lib/ep371_music_player/main_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class MusicPlayerApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
