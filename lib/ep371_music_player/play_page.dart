6087ce80e87678b832eb330add2fe11e427a1ad8
Dreamwalker <aristojeff@gmail.com>
2020-02-26 21:31:53 +0900

    Update play_page.dart

--- lib/ep371_music_player/play_page.dart
+++ lib/ep371_music_player/play_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 
 class PlayPage extends StatefulWidget {
   @override
@@ -23,7 +23,7 @@ class _PlayPageState extends State<PlayPage> {
               children: <Widget>[
                 IconButton(
                   icon: Icon(Icons.arrow_back_ios),
-                  onPressed: (){
+                  onPressed: () {
                     Navigator.of(context).pop();
                   },
                 ),
@@ -31,7 +31,6 @@ class _PlayPageState extends State<PlayPage> {
               ],
             ),
           ),
-
           Positioned(
             left: 64,
             right: 64,
@@ -40,18 +39,59 @@ class _PlayPageState extends State<PlayPage> {
               height: 260,
               width: MediaQuery.of(context).size.width - 100,
               decoration: BoxDecoration(
-                color: Colors.blue,
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                  image: NetworkImage("https://cdn.pixabay.com/photo/2017/08/30/12/45/girl-2696947__340.jpg"),
-                  fit: BoxFit.cover
-                )
-              ),
+                  color: Colors.blue,
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: NetworkImage(
+                          "https://cdn.pixabay.com/photo/2017/08/30/12/45/girl-2696947__340.jpg"),
+                      fit: BoxFit.cover)),
               child: Center(
                 child: CircleAvatar(
                   radius: 24,
                   backgroundColor: Colors.yellow,
                 ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 16,
+            right: 16,
+            top: 440,
+            child: Container(
+              height: 180,
+              color: Colors.blue,
+              child: Column(
+                children: <Widget>[
+                  Icon(Icons.music_note),
+                  SizedBox(height: 16,),
+                  Text(
+                    "Sam Fischer",
+                    style: GoogleFonts.montserrat(
+                      fontSize: 19,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "This City",
+                    style: GoogleFonts.montserrat(),
+                  ),
+                  SizedBox(height: 24,),
+                  Container(
+                    height: 52,
+                    color: Colors.red,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text("1:35", style: GoogleFonts.montserrat(
+                        fontWeight: FontWeight.bold
+                      ),),
+                      Text("3:42", style: GoogleFonts.montserrat(
+                          fontWeight: FontWeight.bold
+                      ),)
+                    ],
+                  )
+                ],
               ),
             ),
           )
@@ -60,26 +100,3 @@ class _PlayPageState extends State<PlayPage> {
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
