6db6bccc971f51f94eb98776ab23091ddfde43b0
Dreamwalker <aristojeff@gmail.com>
2020-02-27 22:07:17 +0900

    Update play_page.dart

--- lib/ep371_music_player/play_page.dart
+++ lib/ep371_music_player/play_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_notebook_4th/ep371_music_player/main_page.dart';
@@ -9,8 +11,24 @@ class PlayPage extends StatefulWidget {
 }
 
 class _PlayPageState extends State<PlayPage> {
+  List<int> soundBars = [];
+  double barWidth = 5.0;
+  double soundPosition = 0.0;
+  double nextSoundPosition = 140.0;
+  Random r = Random();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    for (int i = 0; i < 72; i++) {
+      soundBars.add(r.nextInt((52)));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    int i = 0;
     return Scaffold(
       backgroundColor: Colors.yellow,
       body: Stack(
@@ -82,7 +100,21 @@ class _PlayPageState extends State<PlayPage> {
                   ),
                   Container(
                     height: 52,
-                    color: Colors.red,
+                    child: Row(
+                      children: soundBars.map((h) {
+                        Color color = i >= soundPosition / barWidth &&
+                                i <= nextSoundPosition / barWidth
+                            ? backgroundColor
+                            : Colors.grey;
+                        i++;
+                        return Container(
+                          margin: EdgeInsets.only(right: 1),
+                          color: color,
+                          height: h.toDouble(),
+                          width: 4,
+                        );
+                      }).toList(),
+                    ),
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -110,22 +142,35 @@ class _PlayPageState extends State<PlayPage> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Icon(Icons.refresh),
+                IconButton(
+                  padding: EdgeInsets.zero,
+                  icon: Icon(Icons.refresh),
+                  onPressed: (){
+                      soundBars.clear();
+                      for (int i = 0; i < 72; i++) {
+                        soundBars.add(r.nextInt((52)));
+                      }
+                      setState(() {
+
+                      });
+                  },
+                ),
                 Icon(Icons.skip_previous),
                 Card(
-                  color:backgroundColor,
-                  elevation: 12,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16)
-                  ),
-                  child:Container(
-                    height: 84,
-                    width: 48,
-                    child: Center(
-                      child: Icon(Icons.play_arrow, color: Colors.white,),
-                    ),
-                  )
-                ),
+                    color: backgroundColor,
+                    elevation: 12,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16)),
+                    child: Container(
+                      height: 84,
+                      width: 48,
+                      child: Center(
+                        child: Icon(
+                          Icons.play_arrow,
+                          color: Colors.white,
+                        ),
+                      ),
+                    )),
                 Icon(Icons.skip_next),
                 Icon(Icons.shuffle),
               ],
