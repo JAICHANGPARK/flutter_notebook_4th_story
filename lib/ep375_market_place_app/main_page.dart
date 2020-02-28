078d13aeaed7b91130c5caa3de211e983ed601f6
Dreamwalker <aristojeff@gmail.com>
2020-02-28 21:11:30 +0900

    Update main_page.dart

--- lib/ep375_market_place_app/main_page.dart
+++ lib/ep375_market_place_app/main_page.dart
@@ -3,6 +3,52 @@ import 'package:flutter/material.dart';
 class MarketPlaceApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: MarketPlaceHome(),
+    );
   }
 }
+
+class MarketPlaceHome extends StatefulWidget {
+  @override
+  _MarketPlaceHomeState createState() => _MarketPlaceHomeState();
+}
+
+class _MarketPlaceHomeState extends State<MarketPlaceHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+        backgroundColor: Colors.yellow,
+        bottom: PreferredSize(
+          preferredSize: Size(MediaQuery.of(context).size.width, 50),
+          child: Row(),
+        ),
+      ),
+
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
