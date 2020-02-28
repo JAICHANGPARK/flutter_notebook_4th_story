48eb9678e07adbf354300495cde8d6290d066c74
Dreamwalker <aristojeff@gmail.com>
2020-02-28 21:16:58 +0900

    Update main_page.dart

--- lib/ep375_market_place_app/main_page.dart
+++ lib/ep375_market_place_app/main_page.dart
@@ -18,51 +18,45 @@ class _MarketPlaceHomeState extends State<MarketPlaceHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Row(
-          children: <Widget>[
-            IconButton(
-              icon: Icon(Icons.keyboard_arrow_left),
-              onPressed: (){},
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 2,
+            child: Placeholder(),
+          ),
+          Expanded(
+            flex: 6,
+            child: Container(
+              decoration: BoxDecoration(color: Colors.grey),
             ),
-            IconButton(
-              icon: Icon(Icons.menu),
-              onPressed: (){},
+          ),
+          Expanded(
+            flex: 2,
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    child: Center(
+                      child: Text(
+                        "Discounts, free, tax, and tip will be\ncalculated on checkout",
+                        style: TextStyle(),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(color: Colors.yellow),
+                    child: Center(
+                      child: Text("CHECK OUT NOW WITH ELIGIBLE SHOPS"),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-        title: Text("SHOPPING CART", style: TextStyle(
-          color: Colors.black
-        ),),
-        backgroundColor: Colors.yellow,
-        bottom: PreferredSize(
-          preferredSize: Size(MediaQuery.of(context).size.width, 50),
-          child: Row(),
-        ),
+          ),
+        ],
       ),
-
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
