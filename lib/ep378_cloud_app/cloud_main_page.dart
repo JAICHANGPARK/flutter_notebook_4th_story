6f473f7f3778da38cfd372ee00dbd70963049625
Dreamwalker <aristojeff@gmail.com>
2020-03-02 20:51:27 +0900

    Update cloud_main_page.dart

--- lib/ep378_cloud_app/cloud_main_page.dart
+++ lib/ep378_cloud_app/cloud_main_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -16,6 +18,23 @@ class CloudMainPage extends StatefulWidget {
 }
 
 class _CloudMainPageState extends State<CloudMainPage> {
+
+  List<Widget> _backgroundBubble;
+  List<int> r = [];
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    for(int i=0; i < 9; i++){
+      r.add(Random().nextInt(64));
+    }
+    r.forEach(
+        (r){
+          print(r);
+        }
+    );
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,6 +62,25 @@ class _CloudMainPageState extends State<CloudMainPage> {
                       color: Colors.grey,
                       fontSize: 32
                     ),)
+                  ],
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  height: MediaQuery.of(context).size.height - 280,
+                  color: Colors.grey[200],
+                ),
+              ),
+              Positioned(
+                left: 0,
+                right: 0,
+                top: 280,
+                child: Row(
+                  children: <Widget>[
+
                   ],
                 ),
               )
