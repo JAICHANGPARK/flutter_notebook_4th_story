8451f3275cafc19cd41dfb074e7c259a4fde4727
Dreamwalker <aristojeff@gmail.com>
2020-03-02 21:06:28 +0900

    Update cloud_main_page.dart

--- lib/ep378_cloud_app/cloud_main_page.dart
+++ lib/ep378_cloud_app/cloud_main_page.dart
@@ -13,120 +13,139 @@ class CloudManagerApp extends StatelessWidget {
     );
   }
 }
+
 class CloudMainPage extends StatefulWidget {
   @override
   _CloudMainPageState createState() => _CloudMainPageState();
 }
 
 class _CloudMainPageState extends State<CloudMainPage> {
-
   List<Widget> _backgroundBubble;
   List<int> r = [];
+  static const double leftPadding = 24.0;
+  static const double rightPadding = 24.0;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    for(int i=0; i < 9; i++){
+    for (int i = 0; i < 9; i++) {
       int k = 32 + Random().nextInt(32);
       r.add(k);
     }
-    r.forEach(
-        (r){
-          print(r);
-        }
-    );
-
+    r.forEach((r) {
+      print(r);
+    });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-          child: Stack(
-            children: <Widget>[
-              Positioned(
-                left: 24,
-                right: 24,
-                top: 24,
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              left: leftPadding,
+              right: rightPadding,
+              top: 24,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        "Cloud",
+                        style: GoogleFonts.montserrat(
+                            fontSize: 48, fontWeight: FontWeight.bold),
+                      ),
+                      Icon(Icons.more_vert),
+                    ],
+                  ),
+                  Text(
+                    "Observatory",
+                    style: GoogleFonts.montserrat(
+                        color: Colors.grey, fontSize: 32),
+                  )
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                height: MediaQuery.of(context).size.height - 280,
+                color: Colors.grey[200],
+              ),
+            ),
+            Positioned(
+              left: 0,
+              right: 0,
+              top: 210,
+              child: Transform.rotate(
+                angle: -0.1,
+                child: Row(
+                    children: r
+                        .map((e) => Container(
+                              height: e.toDouble(),
+                              width: e.toDouble(),
+                              decoration: BoxDecoration(
+                                  color: Colors.grey[200],
+                                  shape: BoxShape.circle),
+                            ))
+                        .toList()),
+              ),
+            ),
+            Positioned(
+              left: leftPadding,
+              right: rightPadding,
+              top: 170,
+              child: Container(
+                height: 380,
+                color: Colors.blue,
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text("Cloud",style: GoogleFonts.montserrat(
-                          fontSize: 48,
-                          fontWeight: FontWeight.bold
-                        ),),
-                        Icon(Icons.more_vert),
-                      ],
+                    Expanded(
+                      flex: 10,
+                      child: Placeholder(),
                     ),
-                    Text("Observatory",style: GoogleFonts.montserrat(
-                      color: Colors.grey,
-                      fontSize: 32
-                    ),)
+                    Expanded(
+                      flex: 1,
+                      child: Placeholder(),
+                    ),
+                    Expanded(
+                      flex: 6,
+                      child: Placeholder(),
+                    ),
                   ],
                 ),
               ),
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                  height: MediaQuery.of(context).size.height - 280,
-                  color: Colors.grey[200],
+            ),
+            Positioned(
+              left: leftPadding,
+              right: rightPadding,
+              bottom: 0,
+              child: Container(
+                height: 120,
+                decoration: BoxDecoration(
+                  color: Colors.red
                 ),
               ),
-              Positioned(
-                left: 0,
-                right: 0,
-                top: 210,
-                child: Transform.rotate(
-                  angle: -0.2,
-                  child: Row(
-                    children: r.map((e) => Container(
-                      height: e.toDouble(),
-                      width: e.toDouble(),
-                      decoration: BoxDecoration(
-                        color: Colors.red,
-                        shape: BoxShape.circle
-                      ),
-                    )).toList()
-                  ),
-                ),
-              )
-              
-            ],
-          ),
+            ),
+            Positioned(
+              bottom: 16,
+              left: leftPadding,
+              right: rightPadding,
+              child: Container(
+                height: 80,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16)),
+              ),
+            )
+          ],
         ),
+      ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
