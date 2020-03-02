e676707fc0d9395ba63d29e730b15195a50234b8
Dreamwalker <aristojeff@gmail.com>
2020-03-02 20:42:04 +0900

    Create cloud_main_page.dart

---
+++ lib/ep378_cloud_app/cloud_main_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CloudManagerApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CloudMainPage(),
+    );
+  }
+}
+class CloudMainPage extends StatefulWidget {
+  @override
+  _CloudMainPageState createState() => _CloudMainPageState();
+}
+
+class _CloudMainPageState extends State<CloudMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
