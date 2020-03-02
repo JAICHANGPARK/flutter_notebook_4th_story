d57a0f10fbbea9ac750524054506af863737d472
Dreamwalker <aristojeff@gmail.com>
2020-03-02 20:47:06 +0900

    Adds

--- lib/ep378_cloud_app/cloud_main_page.dart
+++ lib/ep378_cloud_app/cloud_main_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CloudManagerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: CloudMainPage(),
     );
   }
@@ -16,7 +18,67 @@ class CloudMainPage extends StatefulWidget {
 class _CloudMainPageState extends State<CloudMainPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: SafeArea(
+          child: Stack(
+            children: <Widget>[
+              Positioned(
+                left: 24,
+                right: 24,
+                top: 24,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text("Cloud",style: GoogleFonts.montserrat(
+                          fontSize: 48,
+                          fontWeight: FontWeight.bold
+                        ),),
+                        Icon(Icons.more_vert),
+                      ],
+                    ),
+                    Text("Observatory",style: GoogleFonts.montserrat(
+                      color: Colors.grey,
+                      fontSize: 32
+                    ),)
+                  ],
+                ),
+              )
+              
+            ],
+          ),
+        ),
+    );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter_notebook_4th/ep375_market_place_app/main_page.dart';
 import 'package:flutter_notebook_4th/ep378_cloud_app/cloud_main_page.dart';
 void main() {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-    statusBarColor: Colors.white
+    statusBarColor:  Colors.grey[50]
   ));
   runApp(CloudManagerApp());
 
