4bcba42110dbb70f91b9ab360879b49f99a23b84
Dreamwalker <aristojeff@gmail.com>
2020-03-02 20:42:28 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:flutter_notebook_4th/ep375_market_place_app/main_page.dart';
+import 'package:flutter_notebook_4th/ep378_cloud_app/cloud_main_page.dart';
 void main() {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-    statusBarColor: Colors.yellow
+    statusBarColor: Colors.white
   ));
-  runApp(MarketPlaceApp());
+  runApp(CloudManagerApp());
 
 }
