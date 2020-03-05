1f25d309166b612274653c723aa15d06afb56a10
Dreamwalker <aristojeff@gmail.com>
2020-03-05 21:09:33 +0900

    Adds

--- lib/ep381_model_app/main_page.dart
+++ lib/ep381_model_app/main_page.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
+
 class ModelApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xffEAE0DF),
+      ),
       home: MainPage(),
     );
   }
@@ -15,9 +20,65 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  AnimationController controller;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 2,
+              child: Row(
+                children: <Widget>[
+                  AnimatedIcon(progress: null, icon: null,
+
+                  ),
+
+                  Container(
+                    width: 48,
+                    decoration: BoxDecoration(
+                      color: Colors.red
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 20,
+              child: Placeholder(),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,11 @@ import 'package:flutter/services.dart';
 
 import 'package:flutter_notebook_4th/ep375_market_place_app/main_page.dart';
 import 'package:flutter_notebook_4th/ep378_cloud_app/cloud_main_page.dart';
+import 'package:flutter_notebook_4th/ep381_model_app/main_page.dart';
 void main() {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-    statusBarColor:  Colors.grey[50]
+    statusBarColor:  Color(0xffEAE0DF)
   ));
-  runApp(CloudManagerApp());
+  runApp(ModelApp());
 
 }
