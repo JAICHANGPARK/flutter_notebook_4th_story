b3381748f2ad6b0cacf6b6e000c8a6b18f4ac4a0
Dreamwalker <aristojeff@gmail.com>
2020-03-07 19:29:32 +0900

    Update main_pge.dart

--- lib/ep383_zomato_app/main_pge.dart
+++ lib/ep383_zomato_app/main_pge.dart
@@ -19,41 +19,30 @@ class ZomatoMainPage extends StatefulWidget {
 }
 
 class _ZomatoMainPageState extends State<ZomatoMainPage> {
+  int pageIdx = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
+        elevation: 8,
+        currentIndex: pageIdx,
+        onTap: (index) {
+          setState(() {
+            pageIdx = index;
+          });
+        },
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.card_giftcard),
-          title: Text("Order")),
           BottomNavigationBarItem(
-            icon: Icon(Icons.directions_run),title: Text("Go Out")
-          ),
+              icon: Icon(Icons.card_giftcard), title: Text("Order")),
           BottomNavigationBarItem(
-              icon: Icon(Icons.ac_unit),title: Text("Gold")
-          ),
+              icon: Icon(Icons.directions_run), title: Text("Go Out")),
           BottomNavigationBarItem(
-              icon: Icon(Icons.add_to_queue),title: Text("Go Out")
-          )
+              icon: Icon(Icons.ac_unit), title: Text("Gold")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_to_queue), title: Text("Sneakpeek"))
         ],
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
