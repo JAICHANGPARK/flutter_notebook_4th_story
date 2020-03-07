559e0ed0788c945fe9d15df38c7a3d1db167a53a
Dreamwalker <aristojeff@gmail.com>
2020-03-07 19:22:59 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-import 'package:flutter_notebook_4th/ep375_market_place_app/main_page.dart';
-import 'package:flutter_notebook_4th/ep378_cloud_app/cloud_main_page.dart';
-import 'package:flutter_notebook_4th/ep381_model_app/main_page.dart';
+import 'package:flutter_notebook_4th/ep383_zomato_app/main_pge.dart';
 void main() {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-    statusBarColor:  Color(0xffEAE0DF)
+      statusBarColor:  Colors.white
   ));
-  runApp(ModelApp());
+  runApp(ZomatoApp());
 
 }
