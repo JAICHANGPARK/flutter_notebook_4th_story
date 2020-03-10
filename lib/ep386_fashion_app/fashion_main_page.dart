0559ab99d77a00108f0026cd3d44cf93c47821da
Dreamwalker <aristojeff@gmail.com>
2020-03-10 19:18:29 +0900

    Update fashion_main_page.dart

--- lib/ep386_fashion_app/fashion_main_page.dart
+++ lib/ep386_fashion_app/fashion_main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class FashionApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -9,6 +8,7 @@ class FashionApp extends StatelessWidget {
     );
   }
 }
+
 class FashionMainPage extends StatefulWidget {
   @override
   _FashionMainPageState createState() => _FashionMainPageState();
@@ -30,15 +30,20 @@ class _FashionMainPageState extends State<FashionMainPage> {
                   child: Row(
                     children: <Widget>[
                       Icon(Icons.menu),
-                      SizedBox(width: 8,),
+                      SizedBox(
+                        width: 8,
+                      ),
                       Icon(Icons.person_outline),
                       Spacer(),
-                      Text("LoveTheSales",style: TextStyle(
-                        fontWeight: FontWeight.bold
-                      ),),
+                      Text(
+                        "LoveTheSales",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
                       Spacer(),
                       Icon(Icons.favorite_border),
-                      SizedBox(width: 8,),
+                      SizedBox(
+                        width: 8,
+                      ),
                       Icon(Icons.search)
                     ],
                   ),
@@ -50,39 +55,68 @@ class _FashionMainPageState extends State<FashionMainPage> {
             flex: 12,
             child: Container(
               decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage("https://cdn.pixabay.com/photo/2016/11/16/10/28/two-girls-1828539_960_720.jpg"),
-                  fit: BoxFit.cover
-                )
-              ),
+                  image: DecorationImage(
+                      image: NetworkImage(
+                          "https://cdn.pixabay.com/photo/2016/10/16/04/05/girl-1744349_960_720.jpg"),
+                      fit: BoxFit.cover)),
             ),
           ),
           Expanded(
             flex: 5,
-            child: Placeholder(),
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Placeholder(),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8),
+                        child: VerticalDivider(),
+                      ),
+                      Expanded(
+                        child: Placeholder(),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8),
+                        child: VerticalDivider(),
+                      ),
+                      Expanded(
+                        child: Placeholder(),
+                      ),
+                    ],
+                  ),
+                ),
+                Divider(),
+                Expanded(
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Placeholder(),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8),
+                        child: VerticalDivider(),
+                      ),
+                      Expanded(
+                        child: Placeholder(),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8),
+                        child: VerticalDivider(),
+                      ),
+                      Expanded(
+                        child: Placeholder(),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
-
         ],
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
