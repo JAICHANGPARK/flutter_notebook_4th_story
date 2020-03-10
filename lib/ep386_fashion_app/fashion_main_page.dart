6e6ba7656fa166550ea8b2383f9cdd81e6edda1e
Dreamwalker <aristojeff@gmail.com>
2020-03-10 19:08:57 +0900

    추가추가

--- lib/ep386_fashion_app/fashion_main_page.dart
+++ lib/ep386_fashion_app/fashion_main_page.dart
@@ -9,3 +9,54 @@ class FashionApp extends StatelessWidget {
     );
   }
 }
+class FashionMainPage extends StatefulWidget {
+  @override
+  _FashionMainPageState createState() => _FashionMainPageState();
+}
+
+class _FashionMainPageState extends State<FashionMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 2,
+            child: Placeholder(),
+          ),
+          Expanded(
+            flex: 2,
+            child: Placeholder(),
+          ),
+          Expanded(
+            flex: 2,
+            child: Placeholder(),
+          ),
+          Expanded(
+            flex: 2,
+            child: Placeholder(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:flutter_notebook_4th/ep383_zomato_app/main_pge.dart';
+import 'package:flutter_notebook_4th/ep386_fashion_app/fashion_main_page.dart';
 void main() {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor:  Colors.white
   ));
-  runApp(ZomatoApp());
+  runApp(FashionApp());
 
 }
