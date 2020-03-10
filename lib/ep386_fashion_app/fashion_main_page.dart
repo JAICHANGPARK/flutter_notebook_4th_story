3cc6888c2340ab51d3fe682e23e1f2e9046794e8
Dreamwalker <aristojeff@gmail.com>
2020-03-10 19:07:00 +0900

    Create fashion_main_page.dart

---
+++ lib/ep386_fashion_app/fashion_main_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class FashionApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FashionMainPage(),
+    );
+  }
+}
