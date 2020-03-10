2a9b0e935bcbedf94cfc5a4daa2c884530483867
Dreamwalker <aristojeff@gmail.com>
2020-03-10 19:32:34 +0900

    Update fashion_main_page.dart

--- lib/ep386_fashion_app/fashion_main_page.dart
+++ lib/ep386_fashion_app/fashion_main_page.dart
@@ -5,6 +5,7 @@ class FashionApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: FashionMainPage(),
     );
   }
