545a654c85fa6ff40a4c2d14afc162b87ce527ae
Dreamwalker <aristojeff@gmail.com>
2020-03-13 20:34:32 +0900

    Update data_model.dart

--- lib/ep387_personal_buyer_assistance/data_model.dart
+++ lib/ep387_personal_buyer_assistance/data_model.dart
@@ -1,20 +1,19 @@
-
-class Popular{
+class Popular {
   String img;
   String title;
   String lowPrice;
   String highPrice;
 
   Popular({this.img, this.title, this.lowPrice, this.highPrice});
-
 }
 
-class Late{
+class Late {
   String img;
   String title;
   String lowPrice;
   String highPrice;
-  String from ;
+  String from;
+
   String request;
   bool isSaved;
 
@@ -22,25 +21,35 @@ class Late{
 }
 
 List<Popular> popularItems = [
-  Popular(img: "",
-   title: "Hanjk Goglio Shoes",
-  lowPrice: "\$240",
-  highPrice: "\$320"),
-  Popular(img: "",
-      title: "Super Dolor Long Pant",
-      lowPrice: "\$170",
-      highPrice: "\$230"),
-  Popular(img: "",
-      title: "Kuplux Merapi Mbake",
-      lowPrice: "\$140",
-      highPrice: "\$220"),
-  Popular(img: "",
-      title: "Hanjk Goglio Shoes",
-      lowPrice: "\$240",
-      highPrice: "\$320"),
-
+  Popular(img: "", title: "Hanjk Goglio Shoes", lowPrice: "\$240", highPrice: "\$320"),
+  Popular(img: "", title: "Super Dolor Long Pant", lowPrice: "\$170", highPrice: "\$230"),
+  Popular(img: "", title: "Kuplux Merapi Mbake", lowPrice: "\$140", highPrice: "\$220"),
+  Popular(img: "", title: "Hanjk Goglio Shoes", lowPrice: "\$240", highPrice: "\$320"),
 ];
 
+List<Late> lateItems = [
+  Late(img: "",
+      title: "Green Salad Agliato",
+      highPrice: "\$320",
+      lowPrice: "\$240",
+      from: "Iceland",
+      isSaved: false,
+      request: "36"),
+  Late(img: "",
+      title: "Dounut Kentang",
+      highPrice: "\$320",
+      lowPrice: "\$240",
+      from: "Iceland",
+      isSaved: true,
+      request: "Argentina"),
+  Late(img: "",
+      title: "Jacket Anti Cool",
+      highPrice: "\$320",
+      lowPrice: "\$240",
+      from: "Iceland",
+      isSaved: false,
+      request: "36"),
+];
 
 
 
