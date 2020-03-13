be73844504f2f3fff47533b4690e3aeaa7a6177d
Dreamwalker <aristojeff@gmail.com>
2020-03-13 20:30:56 +0900

    Update data_model.dart

--- lib/ep387_personal_buyer_assistance/data_model.dart
+++ lib/ep387_personal_buyer_assistance/data_model.dart
@@ -20,3 +20,44 @@ class Late{
 
   Late({this.img, this.title, this.lowPrice, this.highPrice, this.from, this.request, this.isSaved});
 }
+
+List<Popular> popularItems = [
+  Popular(img: "",
+   title: "Hanjk Goglio Shoes",
+  lowPrice: "\$240",
+  highPrice: "\$320"),
+  Popular(img: "",
+      title: "Super Dolor Long Pant",
+      lowPrice: "\$170",
+      highPrice: "\$230"),
+  Popular(img: "",
+      title: "Kuplux Merapi Mbake",
+      lowPrice: "\$140",
+      highPrice: "\$220"),
+  Popular(img: "",
+      title: "Hanjk Goglio Shoes",
+      lowPrice: "\$240",
+      highPrice: "\$320"),
+
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
