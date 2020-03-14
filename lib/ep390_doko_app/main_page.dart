d7b107503d6ba93f006fe0c325d7700f310cf85d
Dreamwalker <aristojeff@gmail.com>
2020-03-14 22:12:51 +0900

    Update main_page.dart

--- lib/ep390_doko_app/main_page.dart
+++ lib/ep390_doko_app/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class DokoApp extends StatelessWidget {
@@ -15,6 +16,8 @@ class DokoDetailPage extends StatefulWidget {
 }
 
 class _DokoDetailPageState extends State<DokoDetailPage> {
+  /// 8 23 90
+  Color appAccentColor = Color(0xff08175A );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,14 +29,24 @@ class _DokoDetailPageState extends State<DokoDetailPage> {
             right: 0,
             child: Container(
               height: MediaQuery.of(context).size.height / 3,
-              color: Colors.indigo,
+              color: appAccentColor,
             ),
           ),
           Positioned(
             left: 0,
-            top: MediaQuery.of(context).size.height / 8,
+            top: MediaQuery.of(context).size.height / 4,
             right: 0,
-            child: Card(),
+            bottom: 0,
+            child: Card(
+              margin: EdgeInsets.all(0),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(32),
+                )
+              ),
+              elevation: 4,
+              child: Column(),
+            ),
           )
         ],
       ),
