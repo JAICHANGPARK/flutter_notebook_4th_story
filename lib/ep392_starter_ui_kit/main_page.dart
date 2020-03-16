72636708ebc0173e9fbd48b086eb90a71fee57d4
Dreamwalker <aristojeff@gmail.com>
2020-03-16 20:09:03 +0900

    Update main_page.dart

--- lib/ep392_starter_ui_kit/main_page.dart
+++ lib/ep392_starter_ui_kit/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
@@ -19,8 +20,57 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: 0,
+            child: Container(
+              color: Colors.black,
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            child: Container(
+              height: MediaQuery.of(context).size.height / 1.9,
+              
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(16),
+                  bottomLeft: Radius.circular(16))
+              ),
+            )
+          ),
+
+        ],
+      ),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
