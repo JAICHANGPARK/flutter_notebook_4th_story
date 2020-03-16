38cbce03000253d878267c6728ffe97109c53703
Dreamwalker <aristojeff@gmail.com>
2020-03-16 20:05:23 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,11 @@ import 'package:flutter/services.dart';
 
 import 'package:flutter_notebook_4th/ep387_personal_buyer_assistance/main_page.dart';
 import 'package:flutter_notebook_4th/ep390_doko_app/main_page.dart';
+import 'package:flutter_notebook_4th/ep392_starter_ui_kit/main_page.dart';
 void main() {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor:  Colors.transparent
   ));
-  runApp(DokoApp());
+  runApp(StarterUiKitApp());
 
 }
