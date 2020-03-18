45f0e98dfb726b7437fa37654a46ab89516701e2
Dreamwalker <aristojeff@gmail.com>
2020-03-18 20:38:50 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,11 @@ import 'package:flutter/services.dart';
 import 'package:flutter_notebook_4th/ep387_personal_buyer_assistance/main_page.dart';
 import 'package:flutter_notebook_4th/ep390_doko_app/main_page.dart';
 import 'package:flutter_notebook_4th/ep392_starter_ui_kit/main_page.dart';
+import 'package:flutter_notebook_4th/ep394_bank_app/main_page.dart';
 void main() {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor:  Colors.transparent
   ));
-  runApp(StarterUiKitApp());
+  runApp(BankApp());
 
 }
