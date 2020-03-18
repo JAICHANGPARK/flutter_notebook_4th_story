8d2210b08fe1db85142932225f7cfa2d4b1b5836
Dreamwalker <aristojeff@gmail.com>
2020-03-18 20:50:47 +0900

    Adds Adds Adddddds

--- lib/ep394_bank_app/main_page.dart
+++ lib/ep394_bank_app/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class BankApp extends StatelessWidget {
@@ -8,6 +9,7 @@ class BankApp extends StatelessWidget {
     );
   }
 }
+
 class BudgetPage extends StatefulWidget {
   @override
   _BudgetPageState createState() => _BudgetPageState();
@@ -16,7 +18,56 @@ class BudgetPage extends StatefulWidget {
 class _BudgetPageState extends State<BudgetPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                height: MediaQuery.of(context).size.height / 2.9,
+                decoration: BoxDecoration(
+                    color: Color(0xff453deb),
+
+                    /// 69 61 235
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(48),
+                      topLeft: Radius.circular(48),
+                    )),
+                padding: EdgeInsets.all(24),
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: Text(
+                    "Set your budget",
+                    style: Theme.of(context).textTheme.headline6.copyWith(
+                          color: Colors.white,
+                        ),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                height: MediaQuery.of(context).size.height / 4,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(48),
+                      topLeft: Radius.circular(48),
+                    )),
+                child: Column(
+                  children: <Widget>[],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
-
