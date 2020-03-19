441e92436230b9191e2440d3c062c7f98a99140d
Dreamwalker <aristojeff@gmail.com>
2020-03-19 20:58:16 +0900

    Update main_page.dart

--- lib/ep394_bank_app/main_page.dart
+++ lib/ep394_bank_app/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:bezier_chart/bezier_chart.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -32,6 +33,7 @@ class _BudgetPageState extends State<BudgetPage> {
                 height: MediaQuery.of(context).size.height / 2.3,
                 decoration: BoxDecoration(
                     color: Color(0xff453deb),
+
                     /// 69 61 235
                     borderRadius: BorderRadius.only(
                       topRight: Radius.circular(48),
@@ -79,32 +81,38 @@ class _BudgetPageState extends State<BudgetPage> {
                             return Container(
                               width: 140,
                               margin: EdgeInsets.all(16),
-                              decoration: BoxDecoration(border: Border.all(color: Colors.grey[400]),
-                              borderRadius: BorderRadius.circular(24)),
+                              decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.grey[400]), borderRadius: BorderRadius.circular(24)),
                               padding: EdgeInsets.all(18),
                               child: Column(
                                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                                 children: <Widget>[
                                   Row(
                                     children: <Widget>[
-                                      Container(height: 42,width: 42,
-                                      decoration: BoxDecoration(
-                                          color: Colors.grey[300],
-                                        borderRadius: BorderRadius.circular(8)
-                                      ),),
+                                      Container(
+                                        height: 42,
+                                        width: 42,
+                                        decoration: BoxDecoration(
+                                            color: Colors.grey[300], borderRadius: BorderRadius.circular(8)),
+                                      ),
                                       Padding(
                                         padding: const EdgeInsets.all(16.0),
-                                        child: Text("72%", style: TextStyle(
-                                          color: Color(0xff453deb),
-                                          fontWeight: FontWeight.bold,
-
-                                        ),),
+                                        child: Text(
+                                          "72%",
+                                          style: TextStyle(
+                                            color: Color(0xff453deb),
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
                                       )
                                     ],
                                   ),
-                                  Text("Restaurant",style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                  ),)
+                                  Text(
+                                    "Restaurant",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  )
                                 ],
                               ),
                             );
@@ -119,22 +127,52 @@ class _BudgetPageState extends State<BudgetPage> {
               right: 0,
               top: 0,
               child: Container(
-                height: MediaQuery.of(context).size.height /2,
+                height: MediaQuery.of(context).size.height / 2,
                 child: Column(
                   children: <Widget>[
                     Spacer(),
-                    Text("\$600.00",style: GoogleFonts.montserrat(
-                      fontWeight: FontWeight.bold,
-                      fontSize:42
-                    ),),
-                    Text("March Expenses", style: GoogleFonts.montserrat(
-                      color: Colors.grey,
-                    ),),
+                    Text(
+                      "\$600.00",
+                      style: GoogleFonts.montserrat(fontWeight: FontWeight.bold, fontSize: 42),
+                    ),
+                    Text(
+                      "March Expenses",
+                      style: GoogleFonts.montserrat(
+                        color: Colors.grey,
+                      ),
+                    ),
                     Spacer(),
                     Container(
-                      height: MediaQuery.of(context).size.height / 3.8,
-                      color: Colors.blue,
-                    )
+                        height: MediaQuery.of(context).size.height / 3.8,
+                        width: MediaQuery.of(context).size.width,
+                        child: BezierChart(
+                          bezierChartScale: BezierChartScale.CUSTOM,
+                          xAxisCustomValues: [0, 5, 10, 15, 20, 25, 30],
+
+                          series: [
+                            BezierLine(
+                              label: "Budget",
+                                lineColor: Color(0xff453deb),
+                                dataPointStrokeColor: Color(0xff453deb),
+                                data: [
+                              DataPoint<double>(value: 450.0, xAxis: 11),
+                              DataPoint<double>(value: 600.0, xAxis: 12),
+                              DataPoint<double>(value: 620.0, xAxis: 13),
+                              DataPoint<double>(value: 350.0, xAxis: 14),
+                              DataPoint<double>(value: 480.0, xAxis: 15),
+                              DataPoint<double>(value: 520.0, xAxis: 16),
+                              DataPoint<double>(value: 320.0, xAxis: 17),
+                            ]),
+                          ],
+                          config: BezierChartConfig(
+                            xLinesColor: Color(0xff453deb),
+                            verticalIndicatorStrokeWidth: 3.0,
+                            verticalIndicatorColor: Colors.black,
+                            showVerticalIndicator: true,
+                            bubbleIndicatorColor: Color(0xff453deb),
+
+                          ),
+                        ))
                   ],
                 ),
               ),
@@ -145,27 +183,3 @@ class _BudgetPageState extends State<BudgetPage> {
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
