54fcc31fd980ed5078d80d0bd5e2b1b8ff2590e4
Dreamwalker <aristojeff@gmail.com>
2020-03-20 21:33:54 +0900

    Update main_page.dart

--- lib/ep396_calendar_app/main_page.dart
+++ lib/ep396_calendar_app/main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class CalendarApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -25,24 +24,65 @@ class _CalendarMainPageState extends State<CalendarMainPage> {
         iconTheme: IconThemeData(
           color: Colors.black,
         ),
-        title: Text("August",style: TextStyle(
-          color: Colors.black,
-        ),),
+        title: Text(
+          "August",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.calendar_today),
-            onPressed: (){
-              showDatePicker(context: context, initialDate: DateTime.now(),
-                  firstDate: DateTime(1990, 1,1), lastDate: DateTime(2022, 1,1));
+            onPressed: () {
+              showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(1990, 1, 1),
+                  lastDate: DateTime(2022, 1, 1));
             },
           )
         ],
       ),
       drawer: Drawer(),
-      bo
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 24),
+            height: MediaQuery.of(context).size.height / 8,
+            child: Row(
+              children: <Widget>[
+                Text(
+                  "17",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 34,
+                  ),
+                ),
+                Text(
+                  "August",
+                  style: TextStyle(
+                    fontSize: 34,
+                  ),
+                ),
+                Spacer(),
+                MaterialButton(
+                  onPressed: () {},
+                  color: Colors.orange,
+                  child: Text("Meeting", style: TextStyle(
+                    color: Colors.white,
+                  ),),
+
+                )
+              ],
+            ),
+          ),
+          Divider(height: 0,color: Colors.grey,),
+        ],
+      ),
     );
   }
 }
+
 
 
 
