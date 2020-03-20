b1154ffc58e9824db5eb64c8046550d590727c93
Dreamwalker <aristojeff@gmail.com>
2020-03-20 21:25:15 +0900

    Update main_page.dart

--- lib/ep396_calendar_app/main_page.dart
+++ lib/ep396_calendar_app/main_page.dart
@@ -4,6 +4,46 @@ import 'package:flutter/material.dart';
 class CalendarApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: CalendarMainPage(),
+    );
   }
 }
+
+class CalendarMainPage extends StatefulWidget {
+  @override
+  _CalendarMainPageState createState() => _CalendarMainPageState();
+}
+
+class _CalendarMainPageState extends State<CalendarMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
