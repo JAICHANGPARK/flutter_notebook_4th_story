b5f244fcb952180ebd9672d36ef45f5fd89768ac
Dreamwalker <aristojeff@gmail.com>
2020-03-21 20:07:10 +0900

    Create main_page.dart

---
+++ lib/ep397_marvie_app/main_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MarvieApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MarvieHome(),
+    );
+  }
+}
+class MarvieHome extends StatefulWidget {
+  @override
+  _MarvieHomeState createState() => _MarvieHomeState();
+}
+
+class _MarvieHomeState extends State<MarvieHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
