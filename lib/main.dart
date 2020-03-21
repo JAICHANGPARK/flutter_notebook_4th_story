626dac006838a0b91a68d961308bee2b829ae06c
Dreamwalker <aristojeff@gmail.com>
2020-03-21 20:07:29 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,11 @@ import 'package:flutter_notebook_4th/ep390_doko_app/main_page.dart';
 import 'package:flutter_notebook_4th/ep392_starter_ui_kit/main_page.dart';
 import 'package:flutter_notebook_4th/ep394_bank_app/main_page.dart';
 import 'package:flutter_notebook_4th/ep396_calendar_app/main_page.dart';
+import 'package:flutter_notebook_4th/ep397_marvie_app/main_page.dart';
 void main() {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor:  Colors.transparent
   ));
-  runApp(CalendarApp());
+  runApp(MarvieApp());
 
 }
