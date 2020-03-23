1ef3afe4bd7013475fadbe5094341732b8c48b4f
Dreamwalker <aristojeff@gmail.com>
2020-03-23 20:41:03 +0900

    Add s chart

--- lib/ep399_digital_walet_app/main_page.dart
+++ lib/ep399_digital_walet_app/main_page.dart
@@ -17,7 +17,65 @@ class MyWalletHome extends StatefulWidget {
 class _MyWalletHomeState extends State<MyWalletHome> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: SafeArea(
+              child:
+              Container(
+                height: MediaQuery.of(context).size.height / 5,
+                color: Colors.blue,
+              ),
+            ),
+
+          )
+        ],
+      ),
+
+    );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-import 'package:flutter_notebook_4th/ep387_personal_buyer_assistance/main_page.dart';
-import 'package:flutter_notebook_4th/ep390_doko_app/main_page.dart';
-import 'package:flutter_notebook_4th/ep392_starter_ui_kit/main_page.dart';
-import 'package:flutter_notebook_4th/ep394_bank_app/main_page.dart';
-import 'package:flutter_notebook_4th/ep396_calendar_app/main_page.dart';
-import 'package:flutter_notebook_4th/ep397_marvie_app/main_page.dart';
+
+import 'package:flutter_notebook_4th/ep399_digital_walet_app/main_page.dart';
 void main() {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor:  Colors.transparent
   ));
-  runApp(MarvieApp());
+  runApp(DigitalWalletApp());
 
 }
