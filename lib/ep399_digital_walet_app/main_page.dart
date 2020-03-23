db50f054cbb4b5cfa1139bebf4ad0adb6cdc481b
Dreamwalker <aristojeff@gmail.com>
2020-03-23 20:38:58 +0900

    Update main_page.dart

--- lib/ep399_digital_walet_app/main_page.dart
+++ lib/ep399_digital_walet_app/main_page.dart
@@ -4,6 +4,20 @@ import 'package:flutter/material.dart';
 class DigitalWalletApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: MyWalletHome(),
+    );
   }
 }
+class MyWalletHome extends StatefulWidget {
+  @override
+  _MyWalletHomeState createState() => _MyWalletHomeState();
+}
+
+class _MyWalletHomeState extends State<MyWalletHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
