7419c2e6a8557489d49f182536105ab6694f8beb
Dreamwalker <aristojeff@gmail.com>
2020-03-23 20:41:15 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_notebook_4th/ep399_digital_walet_app/main_page.dart';
 void main() {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      statusBarColor:  Colors.transparent
+      statusBarColor:  Colors.blue
   ));
   runApp(DigitalWalletApp());
 
