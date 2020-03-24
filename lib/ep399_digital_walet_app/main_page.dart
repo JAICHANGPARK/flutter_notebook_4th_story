4776c41831afa43f5702a23896d2353bd84a74b6
Dreamwalker <aristojeff@gmail.com>
2020-03-24 21:08:54 +0900

    Update main_page.dart

--- lib/ep399_digital_walet_app/main_page.dart
+++ lib/ep399_digital_walet_app/main_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
 class DigitalWalletApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,6 +11,7 @@ class DigitalWalletApp extends StatelessWidget {
     );
   }
 }
+
 class MyWalletHome extends StatefulWidget {
   @override
   _MyWalletHomeState createState() => _MyWalletHomeState();
@@ -28,50 +28,44 @@ class _MyWalletHomeState extends State<MyWalletHome> {
             left: 0,
             right: 0,
             child: SafeArea(
-              child:
-              Container(
+              child: Container(
                 height: MediaQuery.of(context).size.height / 3.5,
                 decoration: BoxDecoration(
                     color: Colors.blueAccent,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(24),
-                    bottomRight: Radius.circular(24),
-                  )
-                ),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(24),
+                      bottomRight: Radius.circular(24),
+                    )),
               ),
             ),
           ),
           Positioned(
             left: 16,
             right: 16,
-            top: MediaQuery.of(context).size.height/4.7,
+            top: MediaQuery.of(context).size.height / 4.7,
             bottom: 0,
             child: ListView(
               children: <Widget>[
                 Container(
                   height: MediaQuery.of(context).size.height / 3.5,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(16),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.2),
-                        blurRadius: 2,
-                        spreadRadius: 2,
-                      )
-                    ]
-                  ),
+                  decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(16), boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      blurRadius: 2,
+                      spreadRadius: 2,
+                    )
+                  ]),
                   child: Stack(
                     children: <Widget>[
-                        Positioned(
-                          right: 16,
-                          top: 0,
-                          bottom: 0,
-                          child: Container(
-                            padding: EdgeInsets.all(8),
-                            width: MediaQuery.of(context).size.width / 2,
-                            child: PieChart(
-                              PieChartData(
+                      Positioned(
+                        right: 16,
+                        top: 0,
+                        bottom: 0,
+                        child: Container(
+                          padding: EdgeInsets.all(8),
+                          width: MediaQuery.of(context).size.width / 2,
+                          child: PieChart(
+                            PieChartData(
                                 centerSpaceRadius: 40,
                                 borderData: FlBorderData(
                                   show: false,
@@ -79,89 +73,85 @@ class _MyWalletHomeState extends State<MyWalletHome> {
                                 sectionsSpace: 0,
                                 startDegreeOffset: -120,
                                 sections: [
-                                  PieChartSectionData(value: 35, color: Colors.indigo,
-                                  title: "35%"),
-                                  PieChartSectionData(value: 15, color: Colors.blue,title: "15%"),
-                                  PieChartSectionData(value: 45, color: Colors.yellow,title: "45%"),
-                                ]
-                              ),
-                            ),
+                                  PieChartSectionData(value: 35, color: Colors.indigo, title: "35%"),
+                                  PieChartSectionData(value: 15, color: Colors.blue, title: "15%"),
+                                  PieChartSectionData(value: 45, color: Colors.yellow, title: "45%"),
+                                ]),
                           ),
                         ),
+                      ),
                       Positioned(
                         top: 16,
                         left: 16,
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
-                            Text("\$498.57",style: GoogleFonts.montserrat(
-                              fontSize: 28,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.indigo
-                            ),),
-                            Text("Available Balance",style: TextStyle(
-                              fontSize: 12,
-                              color: Colors.grey
-                            ),),
-                            SizedBox(height: 16,),
+                            Text(
+                              "\$498.57",
+                              style: GoogleFonts.montserrat(
+                                  fontSize: 28, fontWeight: FontWeight.bold, color: Colors.indigo),
+                            ),
+                            Text(
+                              "Available Balance",
+                              style: TextStyle(fontSize: 12, color: Colors.grey),
+                            ),
+                            SizedBox(
+                              height: 16,
+                            ),
                             Row(
                               children: <Widget>[
                                 Container(
                                   height: 8,
                                   width: 8,
-                                  decoration: BoxDecoration(
-                                    color: Colors.indigo,
-                                    shape: BoxShape.circle
-                                  ),
+                                  decoration: BoxDecoration(color: Colors.indigo, shape: BoxShape.circle),
                                 ),
-                                SizedBox(width: 4,),
-                                Text("Spent",style: TextStyle(
-                                  fontSize: 12,
-                                  color: Colors.grey
-                                ),),
-
+                                SizedBox(
+                                  width: 4,
+                                ),
+                                Text(
+                                  "Spent",
+                                  style: TextStyle(fontSize: 12, color: Colors.grey),
+                                ),
                               ],
                             ),
                             Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 12,vertical: 4),
-                              child: Text("\$1.598",style: GoogleFonts.montserrat(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18,
-                                color: Colors.indigo
-                              ),),
+                              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                              child: Text(
+                                "\$1.598",
+                                style: GoogleFonts.montserrat(
+                                    fontWeight: FontWeight.bold, fontSize: 18, color: Colors.indigo),
+                              ),
                             ),
-                            SizedBox(height: 16,),
+                            SizedBox(
+                              height: 16,
+                            ),
                             Row(
                               children: <Widget>[
                                 Container(
                                   height: 8,
                                   width: 8,
-                                  decoration: BoxDecoration(
-                                      color: Colors.yellow,
-                                      shape: BoxShape.circle
-                                  ),
+                                  decoration: BoxDecoration(color: Colors.yellow, shape: BoxShape.circle),
                                 ),
-                                SizedBox(width: 4,),
-                                Text("Earned",style: TextStyle(
-                                    fontSize: 12,
-                                    color: Colors.grey
-                                ),),
-
+                                SizedBox(
+                                  width: 4,
+                                ),
+                                Text(
+                                  "Earned",
+                                  style: TextStyle(fontSize: 12, color: Colors.grey),
+                                ),
                               ],
                             ),
                             Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 12,vertical: 4),
-                              child: Text("\$5.984",style: GoogleFonts.montserrat(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18,
-                                  color: Colors.indigo
-                              ),),
+                              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                              child: Text(
+                                "\$5.984",
+                                style: GoogleFonts.montserrat(
+                                    fontWeight: FontWeight.bold, fontSize: 18, color: Colors.indigo),
+                              ),
                             )
-
                           ],
                         ),
                       )
-
                     ],
                   ),
                 )
@@ -170,47 +160,6 @@ class _MyWalletHomeState extends State<MyWalletHome> {
           )
         ],
       ),
-
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
